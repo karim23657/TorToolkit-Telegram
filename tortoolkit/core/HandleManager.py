c1fb3b0dd1befefec64d20873b6528974ee3e640
karim23657 <64953489+karim23657@users.noreply.github.com>
2021-02-11 23:31:43 +0330

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -185,57 +185,9 @@ async def handle_leech_command(e):
     if not e.is_reply:
         await e.reply("Reply to a link or magnet")
     else:
-        rclone = False
-        tsp = time.time()
-        buts = [[KeyboardButtonCallback("To Telegram",data=f"leechselect tg {tsp}")]]
-        if await get_config() is not None:
-            buts.append(
-                [KeyboardButtonCallback("To Drive",data=f"leechselect drive {tsp}")]
-            )
-        # tsp is used to split the callbacks so that each download has its own callback
-        # cuz at any time there are 10-20 callbacks linked for leeching XD
-           
-        buts.append(
-                [KeyboardButtonCallback("Upload in a ZIP.[Toggle]", data=f"leechzip toggle {tsp}")]
-        )
-        buts.append(
-                [KeyboardButtonCallback("Extract from Archive.[Toggle]", data=f"leechzipex toggleex {tsp}")]
-        )
+        await e.reply(e.text,parse_mode="html")
         
-        conf_mes = await e.reply("<b>First click if you want to zip the contents or extract as an archive (only one will work at a time) then. </b>\n<b>Choose where to uploadyour files:- </b>\nThe files will be uploaded to default destination after 60 sec of no action by user.\n\n Supported Archives to extract .zip, 7z, tar, gzip2, iso, wim, rar, tar.gz,tar.bz2",parse_mode="html",buttons=buts)
-        
-        # zip check in background
-        ziplist = await get_zip_choice(e,tsp)
-        zipext = await get_zip_choice(e,tsp,ext=True)
-        
-        # blocking leech choice 
-        choice = await get_leech_choice(e,tsp)
-        
-        # zip check in backgroud end
-        await get_zip_choice(e,tsp,ziplist,start=False)
-        await get_zip_choice(e,tsp,zipext,start=False,ext=True)
-        is_zip = ziplist[1]
-        is_ext = zipext[1]
-        
-        
-        # Set rclone based on choice
-        if choice == "drive":
-            rclone = True
-        else:
-            rclone = False
-        
-        await conf_mes.delete()
-
-        if rclone:
-            if get_val("RCLONE_ENABLED"):
-                await check_link(e,rclone, is_zip, is_ext)
-            else:
-                await e.reply("<b>DRIVE IS DISABLED BY THE ADMIN</b>",parse_mode="html")
-        else:
-            if get_val("LEECH_ENABLED"):
-                await check_link(e,rclone, is_zip, is_ext)
-            else:
-                await e.reply("<b>TG LEECH IS DISABLED BY THE ADMIN</b>",parse_mode="html")
+                
 
 
 async def get_leech_choice(e,timestamp):
